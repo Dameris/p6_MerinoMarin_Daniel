176723fd66d8b2ca5b63154a71838af252ce7ab7
Daniel Merino <dmermar2302@g.educaand.es>
2024-02-01 14:28:09 +0100

    14:28

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,13 +2,13 @@
     <header>
         <div class="headerTop">
             <p class="headerLinks">
-                Favorites &nbsp;
-                About Us
+                FAVORITES
+                ABOUT US
             </p>
             <img src="../img/logo_infoDisney.png" alt="InfoDisney Logo">
             <p>
-                LogIn
-                SignUp
+                LOG IN
+                SIGN UP
             </p>
         </div>
         <div class="headerBottom">
@@ -25,68 +25,6 @@ export default {
 }
 </script>
 
-<style>
-.headerTop,
-.headerBottom {
-    color: #000;
-    display: flex;
-    justify-content: space-between;
-    padding-left: 1vw;
-    padding-right: 1vw;
-}
-
-.headerTop {
-    background-color: #fff;
-    align-items: center;
-    min-height: 10vh;
-}
-
-.headerBottom {
-    background-color: #FF5757;
-    min-height: 5vh;
-    align-items: center;
-}
-
-/* Button Switch for Dark/Light Mode */
-.btnSwitch {
-    display: inline-block;
-    width: 55px;
-    height: 30px;
-    border-radius: 100px;
-    cursor: pointer;
-    position: relative;
-}
-
-#__btnSwitch~.btnSwitch {
-    background: #fff;
-    transition: all ease 0.6s;
-}
-
-.btnSwitch::after {
-    position: absolute;
-    content: "";
-    width: 22px;
-    height: 22px;
-    background: #000;
-    border-radius: 100px;
-    top: 4px;
-    left: 5px;
-    transition: all ease 0.6s;
-}
-
-#__btnSwitch:checked~.btnSwitch::after {
-    left: 28px;
-    background: #fff;
-    transition: all ease 0.6s;
-}
-
-#__btnSwitch:checked~.btnSwitch {
-    background: #000;
-    transition: all ease 0.6s;
-}
-
-#__btnSwitch {
-    display: none;
-}
-/* End of Button Switch */
+<style lang="scss">
+@import url("../assets/css/source/layout/_header.scss");
 </style>
