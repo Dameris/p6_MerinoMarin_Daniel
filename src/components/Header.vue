b06d2209eec7778da430f9b4cf4b9a1f0e3b3905
Daniel Merino <dmermar2302@g.educaand.es>
2024-02-01 13:48:35 +0100

    13:48 01/02/2024

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,44 +5,88 @@
                 Favorites &nbsp;
                 About Us
             </p>
-            <img src="p6_MerinoMarin_Daniel/src/img/logo_infoDisney.png" alt="InfoDisney Logo">
+            <img src="../img/logo_infoDisney.png" alt="InfoDisney Logo">
             <p>
                 LogIn
                 SignUp
             </p>
         </div>
         <div class="headerBottom">
-            <p>Dark</p>
+            <input type="checkbox" id="__btnSwitch">
+            <label for="__btnSwitch" class="btnSwitch"></label>
             <p>Search</p>
         </div>
     </header>
 </template>
 
 <script>
-    export default {
-        name: "Header"
-    }
+export default {
+    name: "Header"
+}
 </script>
 
 <style>
-    .headerTop,
-    .headerBottom {
-        color: #FF5757;
-        display: flex;
-        justify-content: space-between;
-    }
+.headerTop,
+.headerBottom {
+    color: #000;
+    display: flex;
+    justify-content: space-between;
+    padding-left: 1vw;
+    padding-right: 1vw;
+}
 
-    .headerTop {
-        background-color: #fff;
-    }
+.headerTop {
+    background-color: #fff;
+    align-items: center;
+    min-height: 10vh;
+}
 
-    .headerBottom {
-        background-color: #000;
-    }
+.headerBottom {
+    background-color: #FF5757;
+    min-height: 5vh;
+    align-items: center;
+}
 
-    .headerLinks {
-        padding-left: 1vw;
-    }
+/* Button Switch for Dark/Light Mode */
+.btnSwitch {
+    display: inline-block;
+    width: 55px;
+    height: 30px;
+    border-radius: 100px;
+    cursor: pointer;
+    position: relative;
+}
 
-    
+#__btnSwitch~.btnSwitch {
+    background: #fff;
+    transition: all ease 0.6s;
+}
+
+.btnSwitch::after {
+    position: absolute;
+    content: "";
+    width: 22px;
+    height: 22px;
+    background: #000;
+    border-radius: 100px;
+    top: 4px;
+    left: 5px;
+    transition: all ease 0.6s;
+}
+
+#__btnSwitch:checked~.btnSwitch::after {
+    left: 28px;
+    background: #fff;
+    transition: all ease 0.6s;
+}
+
+#__btnSwitch:checked~.btnSwitch {
+    background: #000;
+    transition: all ease 0.6s;
+}
+
+#__btnSwitch {
+    display: none;
+}
+/* End of Button Switch */
 </style>
