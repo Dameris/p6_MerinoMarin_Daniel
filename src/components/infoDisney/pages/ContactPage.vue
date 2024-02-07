7beeb0ddb395154f309d12afa50b21b6407cafca
Daniel Merino <dmermar2302@g.educaand.es>
2024-02-07 10:58:22 +0100

    07/02/2024

--- src/components/infoDisney/pages/ContactPage.vue
+++ src/components/infoDisney/pages/ContactPage.vue
@@ -1,15 +1,127 @@
 <template>
-    <div>
-        CONTACT
-    </div>
+    <section class="box__contact">
+        <img src="../../img/logo_infoDisney.png" alt="InfoDisney Logo">
+        <p class="contact__title">
+            CONTACT WITH <br> <strong>INFO DISNEY</strong>
+        </p>
+        <form class="form__contact" @submit.prevent="submitForm">
+            <input class="form__input" type="email" name="email" id="email" placeholder="EMAIL" required
+                v-model="formData.email">
+            <input class="form__input" type="text" name="first_name" id="first_name" placeholder="FIRST NAME"
+                spellcheck="true" required v-model="formData.firstName" @input="validateName">
+            <input class="form__input" type="text" name="last_name" id="last_name" placeholder="LAST NAME" spellcheck="true"
+                required v-model="formData.lastName" @input="validateName">
+            <textarea class="form__textarea" name="message" id="message" placeholder="YOUR MESSAGE" pattern=".{20,}" spellcheck="true"
+                required v-model="formData.message" @input="validateMessage" />
+            <button class="form__btn" type="submit" name="send" id="send" value="send">SEND</button>
+        </form>
+    </section>
 </template>
 
 <script>
 export default {
-    
+    data() {
+        return {
+            pageTitle: "Contact Info Disney",
+            formData: {
+                email: "",
+                firstName: "",
+                lastName: "",
+                message: ""
+            }
+        }
+    },
+
+    methods: {
+        submitForm() {
+            console.log("Form has been send")
+            console.log("Data", this.formData)
+            this.formData.email = ""
+            this.formData.firstName = ""
+            this.formData.lastName = ""
+            this.formData.message = ""
+
+            // Logic to send data to the backend
+        }
+    }
 }
 </script>
 
 <style>
-    
-</style>
+.box__contact {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    margin-top: 3vh;
+}
+
+.contact__title {
+    padding-top: 2vh;
+    padding-bottom: 1vh;
+    text-align: center;
+    color: #FF5757;
+}
+
+.form__contact {
+    display: flex;
+    flex-direction: column;
+}
+
+.form__input,
+.form__textarea {
+    margin-bottom: 1vh;
+    width: 18vw;
+    padding: 0.8rem;
+    color: rgb(0, 0, 0, 40%);
+    font-family:
+        Inter,
+        -apple-system,
+        BlinkMacSystemFont,
+        'Segoe UI',
+        Roboto,
+        Oxygen,
+        Ubuntu,
+        Cantarell,
+        'Fira Sans',
+        'Droid Sans',
+        'Helvetica Neue',
+        sans-serif;
+}
+
+.form__input,
+.form__textarea,
+.form__btn {
+    border: 0.1rem solid #FF5757;
+    border-radius: 0.2rem;
+}
+
+.form__input:focus,
+.form__textarea:focus {
+    border: 0.2rem solid #FF5757;
+    outline: none;
+}
+
+.form__textarea {
+    min-height: 20vh;
+}
+
+.form__btn {
+    padding: 0.8rem;
+    color: #fff;
+    background-color: #FF5757;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+}
+
+.form__btn:hover {
+    background-color: #FF7777;
+}
+
+.form__btn:focus {
+    outline: none;
+}
+
+.form__btn:active {
+    transform: translateY(1px);
+}</style>
--- src/components/router/router.js
+++ src/components/router/router.js
@@ -1,7 +1,5 @@
 import { createRouter, createWebHashHistory } from "vue-router"
 
-import LogIn from "../auth/LogIn.vue"
-import SignUp from "../auth/SignUp.vue"
 import DisneyCharacter from "../infoDisney/pages/Disney/DisneyCharacter.vue"
 import DisneyPage from "../infoDisney/pages/Disney/DisneyPage.vue"
 import MarvelCharacter from "../infoDisney/pages/Marvel/MarvelCharacter.vue"
@@ -10,13 +8,11 @@ import PixarCharacter from "../infoDisney/pages/Pixar/PixarCharacter.vue"
 import PixarPage from "../infoDisney/pages/Pixar/PixarPage.vue"
 import StarWarsCharacter from "../infoDisney/pages/Star Wars/StarWarsCharacter.vue"
 import StarWarsPage from "../infoDisney/pages/Star Wars/StarWarsPage.vue"
-import AboutPage from "../infoDisney/pages/AboutPage.vue"
 import HomePage from "../infoDisney/pages/HomePage.vue"
 import FavoritesPage from "../infoDisney/pages/FavoritesPage.vue"
 import LayoutPublic from "../infoDisney/layout/LayoutPublic.vue"
 import LayoutPrivate from "../infoDisney/layout/LayoutPrivate.vue"
 import UserProfile from "../infoDisney/pages/UserProfile.vue"
-import ContactPage from "../infoDisney/pages/ContactPage.vue"
 
 const routes = [
     {
@@ -24,8 +20,8 @@ const routes = [
         component: LayoutPublic,
         children: [
             { path: "", component: HomePage },
-            { path: "/logIn", component: LogIn },
-            { path: "/signUp", component: SignUp },
+            { path: "/logIn", component: () => import("../auth/LogIn.vue") },
+            { path: "/signUp", component: () => import("../auth/SignUp.vue") },
             { path: "/disneyPage", component: DisneyPage },
             { path: "/disneyCharacter", component: DisneyCharacter },
             { path: "/pixarPage", component: PixarPage },
@@ -34,14 +30,15 @@ const routes = [
             { path: "/star-warsCharacter", component: StarWarsCharacter },
             { path: "/marvelPage", component: MarvelPage },
             { path: "/marvelCharacter", component: MarvelCharacter },
-            { path: "/about-us", component: AboutPage },
-            { path: "/contact", component: ContactPage }
+            { path: "/about-us", component: () => import("../infoDisney/pages/AboutPage.vue") },
+            { path: "/contact", component: () => import("../infoDisney/pages/ContactPage.vue") }
         ]
     },
     {
         path: "/private",
         component: LayoutPrivate,
         children: [
+            { path: "", component: HomePage },
             { path: "/userProfile", component: UserProfile },
             { path: "/favorites", component: FavoritesPage }
         ]
