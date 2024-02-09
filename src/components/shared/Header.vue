c444d4deb72f7b6ca6d33034fc804c77b5bca632
Daniel Merino <dmermar2302@g.educaand.es>
2024-02-09 13:02:56 +0100

    09/02/2024

--- src/components/shared/Header.vue
+++ src/components/shared/Header.vue
@@ -623,71 +623,29 @@
 		.headerTop {
 			align-items: center;
 			min-height: 5em;
-			max-width: 18.3em;
+			max-width: 25em;
 			background-color: #fff;
 		}
 
-		.header__link {
-			margin-right: 0.5em;
-			color: #ff5757;
-			text-decoration: none;
+		.header__list {
+			flex-direction: column;
+			align-items: center;
+			font-size: small;
 		}
 
-		.header__link:hover {
-			color: #ffafaf;
-		}
-
-		.header__link:focus {
-			outline: none;
+		.header__list li {
+			margin-top: 0.5em;
+			margin-bottom: 0.3em;
 		}
 
 		.header__logo {
-			max-width: 5em;
-		}
-
-		.header__btn--logIn,
-		.header__btn--signUp {
-			padding: 0.6em 1em;
-			margin-right: 0.5em;
-			color: #fff;
-			border: none;
-			border-radius: 0.3em;
-			cursor: pointer;
-			text-decoration: none;
-			transition: background-color 0.3s ease;
-		}
-
-		.header__btn--logIn {
-			background-color: #ffafaf;
-		}
-
-		.header__btn--signUp {
-			background-color: #ff5757;
-		}
-
-		.header__btn--logIn:hover {
-			background-color: #ffcfcf;
-		}
-
-		.header__btn--signUp:hover {
-			background-color: #ff7777;
-		}
-
-		.header__btn--logIn:focus,
-		.header__btn--signUp:focus {
-			outline: none;
-		}
-
-		.header__btn--logIn:active,
-		.header__btn--signUp:active,
-		.header__link:active {
-			transform: translateY(1em);
+			max-width: 10em;
 		}
 
 		.headerBottom {
 			align-items: center;
 			min-height: 3em;
-			max-width: 18.3em;
+			max-width: 25em;
 			background-color: #ff5757;
 		}
 
@@ -695,7 +653,7 @@
 		.header__btn--switch {
 			display: inline-block;
 			position: relative;
-			width: 2em;
+			width: 2.5em;
 			height: 1.5em;
 			border-radius: 100em;
 			cursor: pointer;
@@ -714,34 +672,84 @@
 		}
 
 		#__btnSwitch:checked ~ .header__btn--switch::after {
-			left: 0.8em;
+			left: 1.2em;
 			background: #fff;
 			transition: all ease 0.6s;
 		}
 		/* End of Button Switch */
 
 		.header__searchInput {
-			width: 22em;
-			padding: 0.6em 1em;
-			margin-right: 0.5em;
-			border: none;
-			border-radius: 0.3em;
-			color: #ff5757;
-			background-color: #f5f5f5;
-			box-shadow:
-				0 2em 4em rgba(0, 0, 0, 0.1),
-				background-color 0.3s,
-				box-shadow 0.3s;
+			width: 15em;
+		}
+	}
+
+	@media (max-width: 570px) {
+		.headerTop,
+		.headerBottom {
+			display: flex;
+			justify-content: space-between;
 		}
 
-		.header__searchInput::placeholder {
-			color: #ff5757;
-		}
-
-		.header__searchInput:focus {
-			outline: none;
+		.headerTop {
+			align-items: center;
+			min-height: 5em;
+			max-width: 35.6em;
 			background-color: #fff;
-			box-shadow: 0 4em 8em rgba(0, 0, 0, 0.1);
+		}
+
+		.header__list {
+			flex-direction: column;
+			align-items: center;
+			font-size: small;
+		}
+
+		.header__list li {
+			margin-top: 0.5em;
+			margin-bottom: 0.3em;
+		}
+
+		.header__logo {
+			max-width: 10em;
+		}
+
+		.headerBottom {
+			align-items: center;
+			min-height: 3em;
+			max-width: 35.6em;
+			background-color: #ff5757;
+		}
+
+		/* Button Switch for Dark/Light Mode */
+		.header__btn--switch {
+			display: inline-block;
+			position: relative;
+			width: 2.5em;
+			height: 1.5em;
+			border-radius: 100em;
+			cursor: pointer;
+		}
+
+		.header__btn--switch::after {
+			position: absolute;
+			width: 1em;
+			height: 1em;
+			top: 0.25em;
+			left: 0.2em;
+			content: "";
+			border-radius: 100em;
+			background: #000;
+			transition: all ease 0.6s;
+		}
+
+		#__btnSwitch:checked ~ .header__btn--switch::after {
+			left: 1.2em;
+			background: #fff;
+			transition: all ease 0.6s;
+		}
+		/* End of Button Switch */
+
+		.header__searchInput {
+			width: 15em;
 		}
 	}
 </style>
