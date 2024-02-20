c39b74207de5d2957999defdd74dc5a709f4b135
Daniel Merino <dmermar2302@g.educaand.es>
2024-02-20 10:59:08 +0100

    20/02/2024

--- src/components/infoDisney/pages/HomePage.vue
+++ src/components/infoDisney/pages/HomePage.vue
@@ -8,45 +8,296 @@
 		<section class="home__section">
 			<nav class="home__sectionDisney">
 				<img
-					src="../../img/disney_logo_bg 1.png"
+					src="../../img/disney_logo_bg.png"
 					class="homeSection__titleImage"
 					alt="Section image"
 				/>
-				<ul class="home__section_ul">
+				<ul class="home__section--ul">
 					<li>
-						<img
-							src="../../img/33631_mickey_mouse_character_disney.png"
-							class="homeSection__image"
-							alt="Section image"
-						/>
-						<span class="homeSection__text"
-							><router-link
-								to="disneyCharacter"
-								class="homeSection__link"
-								>Mickey Mouse</router-link
-							></span
-						>
+						<nav class="home__section--li">
+							<img
+								src="../../img/33631_mickey_mouse_character_disney.png"
+								class="homeSection__image"
+								alt="Section image"
+							/>
+							<span class="homeSection__text"
+								><router-link
+									to="disneyCharacter"
+									class="homeSection__link"
+									>MICKEY MOUSE</router-link
+								></span
+							>
+						</nav>
 					</li>
 					<li>
-						<img
-							src="../../img/33631_mickey_mouse_character_disney.png"
-							class="homeSection__image"
-							alt="Section image"
-						/>
+						<nav class="home__section--li">
+							<img
+								src="../../img/33631_mickey_mouse_character_disney.png"
+								class="homeSection__image"
+								alt="Section image"
+							/>
+							<span class="homeSection__text"
+								><router-link
+									to="disneyCharacter"
+									class="homeSection__link"
+									>MINNIE MOUSE</router-link
+								></span
+							>
+						</nav>
 					</li>
 					<li>
-						<img
-							src="../../img/33631_mickey_mouse_character_disney.png"
-							class="homeSection__image"
-							alt="Section image"
-						/>
+						<nav class="home__section--li">
+							<img
+								src="../../img/33631_mickey_mouse_character_disney.png"
+								class="homeSection__image"
+								alt="Section image"
+							/>
+							<span class="homeSection__text"
+								><router-link
+									to="disneyCharacter"
+									class="homeSection__link"
+									>ELSA</router-link
+								></span
+							>
+						</nav>
 					</li>
 					<li>
-						<img
-							src="../../img/33631_mickey_mouse_character_disney.png"
-							class="homeSection__image"
-							alt="Section image"
-						/>
+						<nav class="home__section--li">
+							<img
+								src="../../img/33631_mickey_mouse_character_disney.png"
+								class="homeSection__image"
+								alt="Section image"
+							/>
+							<span class="homeSection__text"
+								><router-link
+									to="disneyCharacter"
+									class="homeSection__link"
+									>STICH</router-link
+								></span
+							>
+						</nav>
+					</li>
+				</ul>
+			</nav>
+
+			<nav class="home__sectionPixar">
+				<img
+					src="../../img/pixar_logo_bg.png"
+					class="homeSection__titleImage"
+					alt="Section image"
+				/>
+				<ul class="home__section--ul">
+					<li>
+						<nav class="home__section--li">
+							<img
+								src="../../img/33631_mickey_mouse_character_disney.png"
+								class="homeSection__image"
+								alt="Section image"
+							/>
+							<span class="homeSection__text"
+								><router-link
+									to="disneyCharacter"
+									class="homeSection__link"
+									>MICKEY MOUSE</router-link
+								></span
+							>
+						</nav>
+					</li>
+					<li>
+						<nav class="home__section--li">
+							<img
+								src="../../img/33631_mickey_mouse_character_disney.png"
+								class="homeSection__image"
+								alt="Section image"
+							/>
+							<span class="homeSection__text"
+								><router-link
+									to="disneyCharacter"
+									class="homeSection__link"
+									>MINNIE MOUSE</router-link
+								></span
+							>
+						</nav>
+					</li>
+					<li>
+						<nav class="home__section--li">
+							<img
+								src="../../img/33631_mickey_mouse_character_disney.png"
+								class="homeSection__image"
+								alt="Section image"
+							/>
+							<span class="homeSection__text"
+								><router-link
+									to="disneyCharacter"
+									class="homeSection__link"
+									>ELSA</router-link
+								></span
+							>
+						</nav>
+					</li>
+					<li>
+						<nav class="home__section--li">
+							<img
+								src="../../img/33631_mickey_mouse_character_disney.png"
+								class="homeSection__image"
+								alt="Section image"
+							/>
+							<span class="homeSection__text"
+								><router-link
+									to="disneyCharacter"
+									class="homeSection__link"
+									>STICH</router-link
+								></span
+							>
+						</nav>
+					</li>
+				</ul>
+			</nav>
+
+			<nav class="home__sectionStarWars">
+				<img
+					src="../../img/starwars_logo_bg.png"
+					class="homeSection__titleImage"
+					alt="Section image"
+				/>
+				<ul class="home__section--ul">
+					<li>
+						<nav class="home__section--li">
+							<img
+								src="../../img/33631_mickey_mouse_character_disney.png"
+								class="homeSection__image"
+								alt="Section image"
+							/>
+							<span class="homeSection__text"
+								><router-link
+									to="disneyCharacter"
+									class="homeSection__link"
+									>MICKEY MOUSE</router-link
+								></span
+							>
+						</nav>
+					</li>
+					<li>
+						<nav class="home__section--li">
+							<img
+								src="../../img/33631_mickey_mouse_character_disney.png"
+								class="homeSection__image"
+								alt="Section image"
+							/>
+							<span class="homeSection__text"
+								><router-link
+									to="disneyCharacter"
+									class="homeSection__link"
+									>MINNIE MOUSE</router-link
+								></span
+							>
+						</nav>
+					</li>
+					<li>
+						<nav class="home__section--li">
+							<img
+								src="../../img/33631_mickey_mouse_character_disney.png"
+								class="homeSection__image"
+								alt="Section image"
+							/>
+							<span class="homeSection__text"
+								><router-link
+									to="disneyCharacter"
+									class="homeSection__link"
+									>ELSA</router-link
+								></span
+							>
+						</nav>
+					</li>
+					<li>
+						<nav class="home__section--li">
+							<img
+								src="../../img/33631_mickey_mouse_character_disney.png"
+								class="homeSection__image"
+								alt="Section image"
+							/>
+							<span class="homeSection__text"
+								><router-link
+									to="disneyCharacter"
+									class="homeSection__link"
+									>STICH</router-link
+								></span
+							>
+						</nav>
+					</li>
+				</ul>
+			</nav>
+
+			<nav class="home__sectionMarvel">
+				<img
+					src="../../img/marvel_logo_bg.png"
+					class="homeSection__titleImage"
+					alt="Section image"
+				/>
+				<ul class="home__section--ul">
+					<li>
+						<nav class="home__section--li">
+							<img
+								src="../../img/33631_mickey_mouse_character_disney.png"
+								class="homeSection__image"
+								alt="Section image"
+							/>
+							<span class="homeSection__text"
+								><router-link
+									to="disneyCharacter"
+									class="homeSection__link"
+									>MICKEY MOUSE</router-link
+								></span
+							>
+						</nav>
+					</li>
+					<li>
+						<nav class="home__section--li">
+							<img
+								src="../../img/33631_mickey_mouse_character_disney.png"
+								class="homeSection__image"
+								alt="Section image"
+							/>
+							<span class="homeSection__text"
+								><router-link
+									to="disneyCharacter"
+									class="homeSection__link"
+									>MINNIE MOUSE</router-link
+								></span
+							>
+						</nav>
+					</li>
+					<li>
+						<nav class="home__section--li">
+							<img
+								src="../../img/33631_mickey_mouse_character_disney.png"
+								class="homeSection__image"
+								alt="Section image"
+							/>
+							<span class="homeSection__text"
+								><router-link
+									to="disneyCharacter"
+									class="homeSection__link"
+									>ELSA</router-link
+								></span
+							>
+						</nav>
+					</li>
+					<li>
+						<nav class="home__section--li">
+							<img
+								src="../../img/33631_mickey_mouse_character_disney.png"
+								class="homeSection__image"
+								alt="Section image"
+							/>
+							<span class="homeSection__text"
+								><router-link
+									to="disneyCharacter"
+									class="homeSection__link"
+									>STICH</router-link
+								></span
+							>
+						</nav>
 					</li>
 				</ul>
 			</nav>
@@ -58,29 +309,52 @@
 	export default {}
 </script>
 
-<style>
+<style scoped>
 	.home__image {
 		width: 100%;
 	}
 
+	.home__sectionDisney,
+	.home__sectionPixar,
+	.home__sectionStarWars,
+	.home__sectionMarvel {
+		min-height: 50vh;
+		padding-right: 5em;
+		padding-left: 5em;
+		text-align: center;
+	}
+
 	.home__sectionDisney {
 		background-color: #20286d;
-		text-align: center;
-		padding-right: 1em;
-		padding-left: 1em;
+	}
+
+	.home__sectionPixar {
+		background-color: #54acfb;
+	}
+
+	.home__sectionStarWars {
+		background-color: #000;
+	}
+
+	.home__sectionMarvel {
+		background-color: #c51d23;
 	}
 
 	.homeSection__titleImage {
 		padding: 1vh;
 	}
 
-	.home__section_ul {
+	.home__section--ul {
 		display: flex;
 		justify-content: space-between;
 		flex-direction: row;
 		margin-bottom: 30vh;
 		color: #fff;
 		list-style: none;
+	}
+
+	.home__section--li {
+		text-align: center;
 	}
 
 	.homeSection__text {
